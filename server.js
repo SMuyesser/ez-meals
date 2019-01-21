702fc300d24fe7767711303111a709e3a3c16a0c
SMuyesser <smuyesser@gmail.com>
2019-01-21 11:07:32 -0800

    Server updated with passport and mongo connection

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const passport = require('passport');
 const path = require('path');
 
 const app = express();
@@ -7,6 +9,27 @@ const app = express();
 // Body Parser Middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+//db Config
+const db = require("./config/keys").mongoURI;
+
+// Connect to MongoDB
+mongoose
+	.connect(
+		db,
+		{ useNewUrlParser: true }
+	)
+	.then(() => console.log("MongoDB Connected"))
+	.catch(err => console.log(err));
+
+// Passport middleware
+app.use(passport.initialize());
+
+// Passport Config
+require("./config/passport")(passport);
+
+// Routes
+app.use("/api/users", users);
 
 // Serve static assets if in production
 if(process.env.NODE_ENV === 'production') {
