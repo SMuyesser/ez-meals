4356fd071e7a2a891249dcef8155bf956a5eefe8
SMuyesser <smuyesser@gmail.com>
2019-01-23 15:55:37 -0800

    actions created for authorization

---
+++ client/src/actions/authActions.js
@@ -0,0 +1,10 @@
+import axios from 'axios';
+import setAuthToken from '../utils/setAuthToken';
+import jwt_decode from 'jwt-decode';
+
+import { GET_ERRORS, SET_CURRENT_USER } from './types';
+
+// Register New User
+// Login get token
+// Set user that logged in
+// log user out
