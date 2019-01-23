1311d57e8f0c6225323f2ee1aabb6b9e182cf937
SMuyesser <smuyesser@gmail.com>
2019-01-23 15:54:45 -0800

    App.js updated with store and router

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,48 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import splashImg from './images/appetizer-bowls-breakfast-1640770 (1).jpg';
-import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import jwt_decode from 'jwt-decode';
+import setAuthToken from './utils/setAuthToken';
+/*import { setCurrentUser, logoutUser} from './actions/authActions.js';*/
+
+import { Provider } from 'react-redux';
+import store from './store';
 
 import Footer from './components/layout/Footer';
+
+import './App.css';
+
+/*// Check for token
+if (localStorage.jwtToken) {
+  // Set auth token header auth
+  setAuthToken(localStorage.jwtToken);
+  // Decode token and get user info and expiration
+  const decoded = jwt_decode(localStorage.jwtToken);
+  // Set user and isAuthenticated
+  store.dispatch(setCurrentUser(decoded));
+
+  // Check for expired token
+  const currentTime = Date.now() / 1000;
+  // if decoded token expiration time is less than currentTime
+  if (decoded.exp < currentTime) {
+    // Logout user
+    store.dispatch(logoutUser());
+    // Clear current profile
+    store.dispatch(clearCurrentProfile());
+    // Redirect to login
+    window.location.href = "/login";
+  }
+}*/
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Footer />
-      </div>
+      <Provider store={store}>
+        <Router>
+          <div className="App">
+            <Footer />
+          </div>
+        </Router>
+      </Provider>
     );
   }
 }
