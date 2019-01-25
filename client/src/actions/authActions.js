0fab167f58c2481d10c1c5cef465b05778c0bb93
SMuyesser <smuyesser@gmail.com>
2019-01-24 16:42:24 -0800

    login user action added

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -19,5 +19,29 @@ export const registerUser = (userData, history) => dispatch => {
 
 
 // Login get token
+export const loginUser = userData => dispatch => {
+	axios
+		.post("/api/users/login", userData)
+		.then(res => {
+			// Save to local storage
+			const { token } = res.data;
+			// Set token to local storage
+			localStorage.setItem("jwtToken", token);
+			// Set token to auth header
+			setAuthToken(token);
+			// Decod token to get user data
+			const decoded = jwt_decode(token);
+			// Set current user
+			dispatch(setCurrentUser(decoded));
+		})
+		.catch(err =>
+			dispatch({
+				type: GET_ERRORS,
+				payload: err.response.data
+			})
+		);
+};
+
+
 // Set user that logged in
 // log user out
