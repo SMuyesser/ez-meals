def5c792123483c537f2fedf3bcf14955bc6ace4
SMuyesser <smuyesser@gmail.com>
2019-01-24 16:41:49 -0800

    register user action

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -5,6 +5,19 @@ import jwt_decode from 'jwt-decode';
 import { GET_ERRORS, SET_CURRENT_USER } from './types';
 
 // Register New User
+export const registerUser = (userData, history) => dispatch => {
+	axios
+		.post("/api/users/register", userData)
+		.then(res => history.push("/login"))
+		.catch(err =>
+			dispatch({
+				type: GET_ERRORS,
+				payload: err.response.data
+			})
+		);
+};
+
+
 // Login get token
 // Set user that logged in
 // log user out
