4e16a49d6396a795057bf49a42f36fd14bd4602c
SMuyesser <smuyesser@gmail.com>
2019-01-24 19:16:18 -0800

    Navbar component created

---
+++ client/src/components/layout/Navbar.jsx
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { logoutUser } from '../../actions/authActions';
+
+class Navbar extends Component {
+	render() {
+		return (
+			<div></div>
+		);
+	}
+}
+
+Navbar.propTypes = {
+	logoutUser: PropTypes.func.isRequired,
+	auth: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+	auth: auth.state
+});
+
+export default connect(mapStateToProps, { logoutUser })(Navbar);
