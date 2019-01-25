a77198f44c10222f4a1666efa0246c3f073aa694
SMuyesser <smuyesser@gmail.com>
2019-01-24 16:50:11 -0800

    uncommented app.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import jwt_decode from 'jwt-decode';
 import setAuthToken from './utils/setAuthToken';
-/*import { setCurrentUser, logoutUser} from './actions/authActions.js';*/
+import { setCurrentUser, logoutUser} from './actions/authActions.js';
 
 import { Provider } from 'react-redux';
 import store from './store';
@@ -11,7 +11,7 @@ import Footer from './components/layout/Footer';
 
 import './App.css';
 
-/*// Check for token
+// Check for token
 if (localStorage.jwtToken) {
   // Set auth token header auth
   setAuthToken(localStorage.jwtToken);
@@ -26,12 +26,10 @@ if (localStorage.jwtToken) {
   if (decoded.exp < currentTime) {
     // Logout user
     store.dispatch(logoutUser());
-    // Clear current profile
-    store.dispatch(clearCurrentProfile());
     // Redirect to login
     window.location.href = "/login";
   }
-}*/
+}
 
 class App extends Component {
   render() {
