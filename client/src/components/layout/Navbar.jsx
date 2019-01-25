2ae478059225c06c5ffecc5e45e7ac5b82751426
SMuyesser <smuyesser@gmail.com>
2019-01-25 09:31:17 -0800

    navbar updated

--- client/src/components/layout/Navbar.jsx
+++ client/src/components/layout/Navbar.jsx
@@ -48,7 +48,7 @@ class Navbar extends Component {
 		);
 
 		return (
-			<nav className="navbar navbar-expand-sm navbar-dark bg-dark mb-4">
+			<nav className="navbar navbar-expand-sm navbar-dark mb-4" id="navbar">
 				<div className="container">
 					<Link className="navbar-brand" to="/">
 						EZ Meals
