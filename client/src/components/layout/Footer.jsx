549554c04f0c0d6a7272cf78cf2bf7b83c4848b8
SMuyesser <smuyesser@gmail.com>
2019-01-25 09:30:50 -0800

    footer updated

--- client/src/components/layout/Footer.jsx
+++ client/src/components/layout/Footer.jsx
@@ -3,7 +3,6 @@ import React from 'react';
 export default () => {
 	return (
  		<footer className="bg-dark text-white mt-5 p-4 text-center">
- 			<div id="edamam-badge" data-color="transparent"></div>
  			Copyright &copy; {new Date().getFullYear()} Sinan Muyesser
  		</footer>
 	);
